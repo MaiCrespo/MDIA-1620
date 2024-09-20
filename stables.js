f471b944c8c0ebb2353b8be1842a6f09e1b09903
Mai Crespo <maicrespo@Mais-MacBook-Pro.local>
2024-09-20 15:35:02 -0700

    initalize stables and add testfile

---
+++ stables.js
@@ -0,0 +1,8 @@
+let horseName = "Benjicles";
+console.log(horseName);
+horseName = "Wilberama";
+console.log(horseName);
+let horseAge = 14;
+console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
