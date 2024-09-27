4244ac2da6ceb7cb0de15f592cff1ccabfc858f7
Mai Crespo <maicrespo@Mais-MacBook-Pro.local>
2024-09-27 15:24:35 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,3 +1,4 @@
+// LESSON-3 Code
 let horseName = "Benjicles";
 console.log(horseName);
 horseName = "Wilberama";
@@ -6,3 +7,14 @@ let horseAge = 14;
 console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside);
+
+//LESSON-4 Code
+console.log(typeof horseName)
+console.log(typeof horseAge)
+const STABLE_MONTHLY_FEE = 100; //uppercase when using const
+console.log(STABLE_MONTHLY_FEE);
+console.log(STABLE_MONTHLY_FEE / 2);
+console.log(STABLE_MONTHLY_FEE * 4);
+console.log("hello " + 8);
+console.log("The stable monthly fee is " + STABLE_MONTHLY_FEE);
+console.log(`My horse's name is "${horseName}" and costs $${STABLE_MONTHLY_FEE} to board him`);
